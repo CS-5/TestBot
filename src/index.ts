cef6907f0ac00bfe3b7f8f465ba875385a2f7927
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2019-07-07 17:30:49 -0400

    Add basic command routing

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,56 @@
+import * as fs from 'fs';
 import * as Discord from 'discord.js';
 import * as dotenv from 'dotenv';
+import { Command } from './models';
+import command from './commands/ping';
 
 const client = new Discord.Client();
+const commands: Discord.Collection<string, Command> = new Discord.Collection();
 dotenv.config();
+
+const PREFIX = process.env.MESSAGE_PREFIX;
+
+if (!PREFIX) {
+  console.warn(
+    `Message prefix not found. Please add an environment variable "MESSAGE_PREFIX", with the desired prefix. (default is '!')`
+  );
+  process.exit();
+}
+
+const commandFiles = fs
+  .readdirSync('./src/commands')
+  .filter(file => file.endsWith('.ts'));
+
+commandFiles.forEach(async filename => {
+  const { default: command } = await import('./commands/' + filename);
+  commands!.set(command.name, command);
+});
 
 client.on('ready', () => {
   console.log('Bot connected');
 });
 
 client.on('message', message => {
-  if (message.content.startsWith('!test')) {
-    message.channel.send("I'm Alive :D");
+  if (!commands) {
+    console.warn(`No commands founds`);
+  }
+  if (!message.content.startsWith(PREFIX as string) || message.author.bot) {
+    return;
+  }
+
+  const args = message.content.slice(PREFIX!.length).split(/ +/);
+  const command = args.shift()!.toLowerCase();
+
+  if (!commands.has(command)) {
+    return;
+  }
+
+  try {
+    const cmd = commands.get(command);
+    cmd && cmd.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error while trying to execute that command');
   }
 });
 
